72d6f2791d2805df4faa753c8a0a886bcd0b78cc
June <ssafy9june@gmail.com>
2024-11-11 22:37:09 +0900

    Learning JAVA

---
+++ yoru_java/src/test/java/com/june/yoru_java/YoruJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.june.yoru_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoruJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
